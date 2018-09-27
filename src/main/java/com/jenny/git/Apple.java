01bf0f0dacda5813bd3f821147afe15946b7564f
Jenny <Jenny@163.com>
2018-09-27 16:23:08 +0800

    pull hot_fix branch to commit

--- src/main/java/com/jenny/git/Apple.java
+++ src/main/java/com/jenny/git/Apple.java
@@ -5,5 +5,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("TestGit...");
 		System.out.println("TestGit2...");
+		System.out.println("edit on hot_fix");
 	}
 }
